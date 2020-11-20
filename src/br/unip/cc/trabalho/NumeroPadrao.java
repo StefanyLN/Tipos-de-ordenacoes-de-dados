a3703a376915b265217680d700a5f4c387f20eb4
StefanyLN <54037591+StefanyLN@users.noreply.github.com>
2020-11-20 17:07:50 -0300

    Create NumeroPadrao.java

---
+++ src/br/unip/cc/trabalho/NumeroPadrao.java
@@ -0,0 +1,6 @@
+package br.unip.cc.trabalho;
+
+public class NumeroPadrao { 
+public static void insere() {
+  for(int i = 0; i < 5000000; i++) Insere.insere(((int)Math.ceil(Math.random()*10000000))); } 
+  }
